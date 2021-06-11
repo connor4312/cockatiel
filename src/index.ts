a50a752915890e001f9670c57ff6e128f62d36ed
Connor Peet <connor@peet.io>
2021-06-10 17:30:01 -0700

    fix: export IDisposable

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export { Event, EventEmitter } from './common/Event';
 export * from './backoff/Backoff';
 export * from './breaker/Breaker';
 export * from './BulkheadPolicy';
 export * from './CancellationToken';
 export * from './CircuitBreakerPolicy';
+export { Event, EventEmitter, IDisposable } from './common/Event';
 export * from './errors/Errors';
 export * from './FallbackPolicy';
+export * from './NoopPolicy';
 export * from './Policy';
 export * from './RetryPolicy';
 export * from './TimeoutPolicy';
-export * from './NoopPolicy';
