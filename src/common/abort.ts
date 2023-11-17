86179888ea118fe12f4c43b202e104ffcb0d2937
Chris Sperry <chris.sperry@outthink.io>
2023-11-17 11:36:14 +0000

    Add return type

--- src/common/abort.ts
+++ src/common/abort.ts
@@ -10,10 +10,15 @@ export const abortedSignal = cancelledSrc.signal;
  * Creates a new AbortController that is aborted when the parent signal aborts.
  * @private
  */
-export const deriveAbortController = (signal?: AbortSignal) => {
+export const deriveAbortController = (
+  signal?: AbortSignal,
+): {
+  controller: AbortController;
+  dispose: () => void;
+} => {
   const ctrl = new AbortController();
   if (!signal) {
-    return { controller: ctrl };
+    return { controller: ctrl, dispose: () => {} };
   }
 
   if (signal.aborted) {
