a7c6053b6b05ffb762e5dfcf2f3ef1a84c934c6c
John Donovan <john@asktia.com>
2024-11-05 22:08:41 -0800

    correct type predicates for errors

--- src/errors/Errors.ts
+++ src/errors/Errors.ts
@@ -17,10 +17,10 @@ export const isBulkheadRejectedError = (e: unknown): e is BulkheadRejectedError 
   !!e && e instanceof Error && 'isBulkheadRejectedError' in e;
 
 export const isIsolatedCircuitError = (e: unknown): e is IsolatedCircuitError =>
-  !!e && e instanceof Error && 'isBulkheadRejectedError' in e;
+  !!e && e instanceof Error && 'isIsolatedCircuitError' in e;
 
 export const isTaskCancelledError = (e: unknown): e is TaskCancelledError =>
-  !!e && e instanceof Error && 'isBulkheadRejectedError' in e;
+  !!e && e instanceof Error && 'isTaskCancelledError' in e;
 
 export const isHydratingCircuitError = (e: unknown): e is HydratingCircuitError =>
   !!e && e instanceof Error && 'isHydratingCircuitError' in e;
